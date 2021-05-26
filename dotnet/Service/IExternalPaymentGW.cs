01bf5b28be952e2a32c8bf2f7428bc46ee9a808c
aneez <aneez.rafeek@gmail.com>
2021-05-26 16:59:19 +0530

    Add service and repo layer

---
+++ dotnet/Service/IExternalPaymentGW.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace server.Service
+{
+    public interface IExternalPaymentGW
+    {
+        void Authorize();
+
+        void Capture();
+
+    }
+
+    public class StripePaymentGW : IExternalPaymentGW
+    {
+
+        public StripePaymentGW()
+        {
+
+        }
+
+        void IExternalPaymentGW.Authorize()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IExternalPaymentGW.Capture()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
